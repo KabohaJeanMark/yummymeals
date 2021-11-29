bf7d174737c126160d47be1e7a0a33262c2b85c6
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-29 17:27:40 +0300

    Add actions for the different types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,22 @@
 const GET_MEALS = 'GET_MEALS';
 const GET_MEALS_SUCCESS = 'GET_MEALS_SUCCESS';
 const GET_MEALS_ERROR = 'GET_MEALS_ERROR';
+
+const getMeals = () => ({
+  type: GET_MEALS,
+});
+
+const getMealsSuccess = (meal) => ({
+  type: GET_MEALS_SUCCESS,
+  meal,
+});
+
+const getMealsError = (error) => ({
+  type: GET_MEALS_ERROR,
+  error,
+});
+
+export {
+  GET_MEALS, GET_MEALS_SUCCESS,
+  GET_MEALS_ERROR, getMeals, getMealsSuccess, getMealsError,
+};
