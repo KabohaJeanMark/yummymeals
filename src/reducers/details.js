86490052ac9cd2b746caf998901137aefde9a8f7
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-30 16:57:08 +0300

    Add a meal details reducer

---
+++ src/reducers/details.js
@@ -0,0 +1,19 @@
+import GET_MEAL_DETAILS from '../actions';
+
+const initialState = {
+
+  mealDetails: [],
+};
+
+const mealDetailsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_MEAL_DETAILS:
+      return {
+        ...state,
+        mealDetails: action.id,
+      };
+    default:
+      return state;
+  }
+};
+export default mealDetailsReducer;
