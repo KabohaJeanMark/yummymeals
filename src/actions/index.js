1b392c4a4ce66047571716f2dbddf4cb0b880a60
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-30 06:50:44 +0300

    use dispatch in our action to get the particular meal category after a successful hit on the API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const GET_MEALS = 'GET_MEALS';
 const GET_MEALS_SUCCESS = 'GET_MEALS_SUCCESS';
 const GET_MEALS_ERROR = 'GET_MEALS_ERROR';
@@ -16,7 +18,16 @@ const getMealsError = (error) => ({
   error,
 });
 
+const fetchCategoryMeals = (category) => (dispatch) => {
+  dispatch(getMeals);
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+  axios.get(url).then((response) => {
+    dispatch(getMealsSuccess(response.data.meals));
+  })
+    .catch((error) => dispatch(getMealsError(error.message)));
+};
+
 export {
   GET_MEALS, GET_MEALS_SUCCESS,
-  GET_MEALS_ERROR, getMeals, getMealsSuccess, getMealsError,
+  GET_MEALS_ERROR, getMeals, getMealsSuccess, getMealsError, fetchCategoryMeals,
 };
