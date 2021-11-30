b510a74fcf0bc936d5f0a9d1ed278b0985d1c7ed
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-30 22:58:35 +0300

    Style the meals container

--- src/containers/Meals.js
+++ src/containers/Meals.js
@@ -18,8 +18,9 @@ const MealsContainer = () => {
   };
 
   return (
-    <>
-      <select name="category" id="category" placeholder="Food category" onChange={(e) => setCategory(e.target.value)}>
+    <div className="container-fluid">
+      <p className="mt-2">Please select a food category from the dropdown :)</p>
+      <select className="form-select m-2" aria-label="Default select example" name="category" id="category" placeholder="Food category" onChange={(e) => setCategory(e.target.value)}>
         <option value="Breakfast">Breakfast</option>
         <option value="Dessert">Dessert</option>
         <option value="Vegan">Vegan</option>
@@ -27,17 +28,18 @@ const MealsContainer = () => {
         <option value="Side">Side</option>
         <option value="Pasta">Pasta</option>
       </select>
-      <div>
+      <div className="mealsContainer">
         {meals && meals.map((meal) => (
-          <div key={meal.idMeal} role="button" tabIndex={0} onClick={() => getMealDetails(meal.idMeal)} onKeyDown={() => getMealDetails(meal.idMeal)}>
-            <Link to={`meals/${meal.idMeal}`}>
-              <img src={meal.strMealThumb} alt={meal.strMeal} />
-              <h3>{meal.strMeal}</h3>
-            </Link>
+          <div key={meal.idMeal} className="card" role="button" tabIndex={0} onClick={() => getMealDetails(meal.idMeal)} onKeyDown={() => getMealDetails(meal.idMeal)}>
+            <img className="card-img-top" src={meal.strMealThumb} alt={meal.strMeal} />
+            <div className="card-body">
+              <h3 className="card-title">{meal.strMeal}</h3>
+              <Link to={`meals/${meal.idMeal}`}>Recipe Details</Link>
+            </div>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
